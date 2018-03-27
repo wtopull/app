7a53f0b3d74a1a4c849b27fdf88c0ddcebcec9a4
wtopull <471601494@qq.com>
2018-03-27 09:00:30 +0800

    aaa

---
+++ config1111/play-config.js
@@ -0,0 +1,348 @@
+(function () {
+  function $_4() {
+    return $_3(8);
+  }
+
+  function $_3(__0) {
+    return {
+      limit: __0
+    };
+  }
+
+  function $_2() {
+    return $_0(_4f);
+  }
+
+  function $_1() {
+    return $_0(_1I);
+  }
+
+  function $_0(__0) {
+    return {
+      tool: __0
+    };
+  }
+
+  var _2 = ["\u4E07\u4F4D", "\u5343\u4F4D", "\u767E\u4F4D", "\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _8 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var _1 = [_2, _8];
+  var _k = ["\u7EC4\u9009120", _8];
+  var _n = ["\u4E8C\u91CD\u53F7", "\u5355\u53F7"];
+  var _m = [_n, _8];
+  var _r = ["\u4E8C\u91CD\u53F7", "\u5355\u53F7"];
+  var _q = [_r, _8];
+  var _v = ["\u4E09\u91CD\u53F7", "\u5355\u53F7"];
+  var _u = [_v, _8];
+  var _z = ["\u4E09\u91CD\u53F7", "\u4E8C\u91CD\u53F7"];
+  var _y = [_z, _8];
+  var _D = ["\u56DB\u91CD\u53F7", "\u5355\u53F7"];
+  var _C = [_D, _8];
+  var _H = ["\u4E07\u4F4D", "\u5343\u4F4D", "\u767E\u4F4D", "\u5341\u4F4D"];
+  var _G = [_H, _8];
+  var _J = ["\u7EC4\u900924", _8];
+  var _M = ["\u4E8C\u91CD\u53F7", "\u5355\u53F7"];
+  var _L = [_M, _8];
+  var _P = ["\u4E8C\u91CD\u53F7", _8];
+  var _S = ["\u4E09\u91CD\u53F7", "\u5355\u53F7"];
+  var _R = [_S, _8];
+  var _W = ["\u5343\u4F4D", "\u767E\u4F4D", "\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _V = [_W, _8];
+  var _Y = ["\u7EC4\u900924", _8];
+  var _11 = ["\u4E8C\u91CD\u53F7", "\u5355\u53F7"];
+  var _10 = [_11, _8];
+  var _14 = ["\u4E8C\u91CD\u53F7", _8];
+  var _17 = ["\u4E09\u91CD\u53F7", "\u5355\u53F7"];
+  var _16 = [_17, _8];
+  var _1b = ["\u4E07\u4F4D", "\u5343\u4F4D", "\u767E\u4F4D"];
+  var _1a = [_1b, _8];
+  var _1d = ["\u5343\u4F4D", "\u767E\u4F4D", "\u5341\u4F4D"];
+  var _1c = [_1d, _8];
+  var _1f = ["\u767E\u4F4D", "\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _1e = [_1f, _8];
+  var _1j = ["\u7EC4\u4E09", _8];
+  var _1l = ["\u7EC4\u4E09", _8];
+  var _1n = ["\u7EC4\u4E09", _8];
+  var _1p = ["\u7EC4\u516D", _8];
+  var _1r = ["\u7EC4\u516D", _8];
+  var _1t = ["\u7EC4\u516D", _8];
+  var _1v = ["\u5C3E\u6570", _8];
+  var _1x = ["\u5C3E\u6570", _8];
+  var _1z = ["\u5C3E\u6570", _8];
+  var _1D = ["\u8C79", "\u987A", "\u5BF9"];
+  var _1I = ["\u5168", "\u6E05"];
+
+  var _1H = $_1();
+
+  var _1B = ["\u7279\u6B8A\u53F7", _1D, _1H];
+  var _1M = ["\u4E07\u4F4D", "\u5343\u4F4D"];
+  var _1L = [_1M, _8];
+  var _1P = ["\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _1O = [_1P, _8];
+  var _1R = ["\u524D\u4E8C\u7EC4\u9009", _8];
+  var _1U = ["\u540E\u4E8C\u7EC4\u9009", _8];
+  var _1Y = ["\u4E07\u4F4D", "\u5343\u4F4D", "\u767E\u4F4D", "\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _1X = [_1Y, _8];
+  var _1Z = ["\u4E0D\u5B9A\u4F4D", _8];
+  var _22 = ["\u4E07\u4F4D", "\u5343\u4F4D", "\u767E\u4F4D"];
+  var _23 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"];
+  var _21 = [_22, _23];
+  var _2g = ["\u5343\u4F4D", "\u767E\u4F4D", "\u5341\u4F4D"];
+  var _2f = [_2g, _23];
+  var _2i = ["\u767E\u4F4D", "\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _2h = [_2i, _23];
+  var _2p = ["\u524D\u4E09\u7EC4\u9009", _23];
+  var _2r = ["\u4E2D\u4E09\u7EC4\u9009", _23];
+  var _2t = ["\u540E\u4E09\u7EC4\u9009", _23];
+  var _2w = ["\u4E07\u4F4D", "\u5343\u4F4D"];
+  var _2v = [_2w, _23];
+  var _2y = ["\u5341\u4F4D", "\u4E2A\u4F4D"];
+  var _2x = [_2y, _23];
+  var _2B = ["\u524D\u4E8C\u7EC4\u9009", _23];
+  var _2D = ["\u540E\u4E8C\u7EC4\u9009", _23];
+  var _2G = ["\u80C6\u7801", "\u62D6\u7801"];
+
+  var _2K = $_0(false);
+
+  var _2J = [_2K];
+  var _2F = [_2G, _23, _2J];
+  var _2M = ["\u524D\u4E09\u4F4D", _23];
+  var _2O = ["\u4E2D\u4E09\u4F4D", _23];
+  var _2Q = ["\u540E\u4E09\u4F4D", _23];
+  var _2T = ["\u7B2C\u4E00\u540D", "\u7B2C\u4E8C\u540D", "\u7B2C\u4E09\u540D"];
+  var _2S = [_2T, _23];
+  var _2Z = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
+  var _2X = ["\u51A0\u519B", _2Z];
+  var _3b = ["\u51A0\u519B", "\u4E9A\u519B"];
+  var _3a = [_3b, _2Z];
+  var _3g = ["\u51A0\u519B", "\u4E9A\u519B", "\u5B63\u519B"];
+  var _3f = [_3g, _2Z];
+  var _3l = ["\u7B2C\u516B\u540D", "\u7B2C\u4E5D\u540D", "\u7B2C\u5341\u540D"];
+  var _3k = [_3l, _2Z];
+  var _3s = ["\u7B2C\u4E00\u540D", "\u7B2C\u4E8C\u540D", "\u7B2C\u4E09\u540D", "\u7B2C\u56DB\u540D"];
+  var _3r = [_3s, _2Z];
+  var _3y = ["\u7B2C\u4E03\u540D", "\u7B2C\u516B\u540D", "\u7B2C\u4E5D\u540D", "\u7B2C\u5341\u540D"];
+  var _3x = [_3y, _2Z];
+  var _3H = ["\u5947", "\u548C", "\u5076"];
+
+  var _3L = $_1();
+
+  var _3F = ["\u5947\u5076\u76D8", _3H, _3L];
+  var _3O = ["\u5927", "\u548C", "\u5C0F"];
+
+  var _3S = $_1();
+
+  var _3M = ["\u548C\u503C\u5927\u5C0F", _3O, _3S];
+  var _3V = ["\u4E0A", "\u4E2D", "\u4E0B"];
+
+  var _3Z = $_1();
+
+  var _3T = ["\u4E0A\u4E0B\u76D8", _3V, _3Z];
+  var _41 = ["\u4E00\u4E2D\u4E00", _23];
+  var _44 = ["\u4E8C\u4E2D\u4E8C", _23];
+  var _47 = ["\u4E09\u4E2D\u4E09", _23];
+  var _4a = ["\u56DB\u4E2D\u56DB", _23];
+  var _4f = ["\u6E05"];
+
+  var _4e = $_2();
+
+  var _4d = [_4e];
+  var _4c = [_2G, _23, _4d];
+  var _4i = ["\u4E94\u4E2D\u4E94", _23];
+
+  var _4m = $_2();
+
+  var _4l = [_4m];
+  var _4k = [_2G, _23, _4l];
+  var _4o = ["\u516D\u4E2D\u4E94", _23];
+
+  var _4s = $_2();
+
+  var _4r = [_4s];
+  var _4q = [_2G, _23, _4r];
+  var _4u = ["\u4E03\u4E2D\u4E94", _23];
+
+  var _4y = $_2();
+
+  var _4x = [_4y];
+  var _4w = [_2G, _23, _4x];
+  var _4A = ["\u516B\u4E2D\u4E94", _23];
+
+  var _4E = $_2();
+
+  var _4D = [_4E];
+  var _4C = [_2G, _23, _4D];
+  var _4G = ["\u4E0A", "\u4E0B"];
+  var _4K = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"];
+  var _5p = ["41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80"];
+  var _4J = [_4K, _5p];
+
+  var _65 = $_2();
+
+  var _66 = $_2();
+
+  var _64 = [_65, _66];
+  var _4F = [_4G, _4J, _64];
+
+  var _69 = $_4();
+
+  var _68 = [_65, _66, _69];
+  var _67 = [_4G, _4J, _68];
+
+  var _6d = $_4();
+
+  var _6c = [_65, _66, _6d];
+  var _6b = [_4G, _4J, _6c];
+
+  var _6h = $_4();
+
+  var _6g = [_65, _66, _6h];
+  var _6f = [_4G, _4J, _6g];
+
+  var _6l = $_4();
+
+  var _6k = [_65, _66, _6l];
+  var _6j = [_4G, _4J, _6k];
+
+  var _6p = $_4();
+
+  var _6o = [_65, _66, _6p];
+  var _6n = [_4G, _4J, _6o];
+
+  var _6t = $_4();
+
+  var _6s = [_65, _66, _6t];
+  var _6r = [_4G, _4J, _6s];
+  var _0 = {
+    wuxing_zhixuan_fs: _1,
+    wuxing_zhixuan_ds: 5,
+    wuxing_zuxuan_120: _k,
+    wuxing_zuxuan_60: _m,
+    wuxing_zuxuan_30: _q,
+    wuxing_zuxuan_20: _u,
+    wuxing_zuxuan_10: _y,
+    wuxing_zuxuan_5: _C,
+    qiansi_zhixuan_fs: _G,
+    qiansi_zhixuan_ds: 4,
+    qiansi_zuxuan_24: _J,
+    qiansi_zuxuan_12: _L,
+    qiansi_zuxuan_6: _P,
+    qiansi_zuxuan_4: _R,
+    housi_zhixuan_fs: _V,
+    housi_zhixuan_ds: 4,
+    housi_zuxuan_24: _Y,
+    housi_zuxuan_12: _10,
+    housi_zuxuan_6: _14,
+    housi_zuxuan_4: _16,
+    qiansan_zhixuan_fs: _1a,
+    zhongsan_zhixuan_fs: _1c,
+    housan_zhixuan_fs: _1e,
+    qiansan_zhixuan_ds: 3,
+    zhongsan_zhixuan_ds: 3,
+    housan_zhixuan_ds: 3,
+    qiansan_zuxuan_zux3: _1j,
+    zhongsan_zuxuan_zux3: _1l,
+    housan_zuxuan_zux3: _1n,
+    qiansan_zuxuan_zux6: _1p,
+    zhongsan_zuxuan_zux6: _1r,
+    housan_zuxuan_zux6: _1t,
+    qiansan_qita_hezhiwz: _1v,
+    zhongsan_qita_hezhiwz: _1x,
+    housan_qita_hezhiwz: _1z,
+    qiansan_qita_bz: _1B,
+    zhongsan_qita_bz: _1B,
+    housan_qita_bz: _1B,
+    qiansan_qita_sz: _1B,
+    zhongsan_qita_sz: _1B,
+    housan_qita_sz: _1B,
+    qiansan_qita_dz: _1B,
+    zhongsan_qita_dz: _1B,
+    housan_qita_dz: _1B,
+    erxing_zhixuan_qianerfs: _1L,
+    erxing_zhixuan_qianerds: 2,
+    erxing_zhixuan_houerfs: _1O,
+    erxing_zhixuan_houerds: 2,
+    erxing_zuxuan_qianerzxfs: _1R,
+    erxing_zuxuan_qianerzxds: 2,
+    erxing_zuxuan_houerzxfs: _1U,
+    erxing_zuxuan_houerzxds: 2,
+    dingweidan_dingweidan_dwq: _1X,
+    budingwei_sanxingbudingwei_qiansanyimabdw: _1Z,
+    budingwei_sanxingbudingwei_qiansanermabdw: _1Z,
+    budingwei_sanxingbudingwei_zhongsanyimabdw: _1Z,
+    budingwei_sanxingbudingwei_housanyimabdw: _1Z,
+    budingwei_sixingbudingwei_qiansiyimabdw: _1Z,
+    budingwei_sixingbudingwei_qiansiermabdw: _1Z,
+    budingwei_wuxingbudingwei_wuxingyimabdw: _1Z,
+    budingwei_wuxingbudingwei_wuxingermabdw: _1Z,
+    budingwei_wuxingbudingwei_wuxingsanmabdw: _1Z,
+    sanma_zhixuan_qiansanzhixuanfs: _21,
+    sanma_zhixuan_zhongsanzhixuanfs: _2f,
+    sanma_zhixuan_housanzhixuanfs: _2h,
+    sanma_zhixuan_qiansanzhixuands: 8,
+    sanma_zhixuan_zhongsanzhixuands: 8,
+    sanma_zhixuan_housanzhixuands: 8,
+    sanma_zuxuan_qiansanzuxuands: 8,
+    sanma_zuxuan_zhongsanzuxuands: 8,
+    sanma_zuxuan_housanzuxuands: 8,
+    sanma_zuxuan_qiansanzuxuanfs: _2p,
+    sanma_zuxuan_zhongsanzuxuanfs: _2r,
+    sanma_zuxuan_housanzuxuanfs: _2t,
+    erma_zhixuan_qianerzhixuanfs: _2v,
+    erma_zhixuan_houerzhixuanfs: _2x,
+    erma_zhixuan_qianerzhixuands: 5,
+    erma_zhixuan_houerzhixuands: 5,
+    erma_zuxuan_qianerzuxuanfs: _2B,
+    erma_zuxuan_houerzuxuanfs: _2D,
+    erma_zuxuan_qianerzuxuandt: _2F,
+    erma_zuxuan_houerzuxuandt: _2F,
+    budingwei_budingwei_qiansanbdw: _2M,
+    budingwei_budingwei_zhongsanbdw: _2O,
+    budingwei_budingwei_housanbdw: _2Q,
+    dingweidan_dingweidan_dwd: _2S,
+    yixing_qianyi_fs: _2X,
+    erxing_qianer_fs: _3a,
+    erxing_qianer_ds: 5,
+    sanxing_qiansan_fs: _3f,
+    sanxing_housan_fs: _3k,
+    sanxing_qiansan_ds: 8,
+    sanxing_housan_ds: 8,
+    sixing_qiansi_fs: _3r,
+    sixing_housi_fs: _3x,
+    sixing_qiansi_ds: 11,
+    sixing_housi_ds: 11,
+    quwei_quwei_ds: _3F,
+    quwei_quwei_dx: _3M,
+    quwei_quwei_sx: _3T,
+    renxuan_renxuands_ds1z1: 2,
+    renxuan_renxuanfs_fs1z1: _41,
+    renxuan_renxuands_ds2z2: 5,
+    renxuan_renxuanfs_fs2z2: _44,
+    dantuo_dantuo_dt2z2: _2F,
+    renxuan_renxuands_ds3z3: 8,
+    renxuan_renxuanfs_fs3z3: _47,
+    dantuo_dantuo_dt3z3: _2F,
+    renxuan_renxuands_ds4z4: 11,
+    renxuan_renxuanfs_fs4z4: _4a,
+    dantuo_dantuo_dt4z4: _4c,
+    renxuan_renxuands_ds5z5: 14,
+    renxuan_renxuanfs_fs5z5: _4i,
+    dantuo_dantuo_dt5z5: _4k,
+    renxuan_renxuands_ds6z5: 17,
+    renxuan_renxuanfs_fs6z5: _4o,
+    dantuo_dantuo_dt6z5: _4q,
+    renxuan_renxuands_ds7z5: 20,
+    renxuan_renxuanfs_fs7z5: _4u,
+    dantuo_dantuo_dt7z5: _4w,
+    renxuan_renxuands_ds8z5: 23,
+    renxuan_renxuanfs_fs8z5: _4A,
+    dantuo_dantuo_dt8z5: _4C,
+    renxuan_renxuan_rx1: _4F,
+    renxuan_renxuan_rx2: _67,
+    renxuan_renxuan_rx3: _6b,
+    renxuan_renxuan_rx4: _6f,
+    renxuan_renxuan_rx5: _6j,
+    renxuan_renxuan_rx6: _6n,
+    renxuan_renxuan_rx7: _6r
+  };
+  module.exports = _0;
+})();
