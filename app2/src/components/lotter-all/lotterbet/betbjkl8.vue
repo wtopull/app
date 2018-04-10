d7cc286b3e6432ea8b385b047a1f425af62329a2
wtopull <471601494@qq.com>
2018-04-10 21:59:20 +0800

    nahtan

--- app2/src/components/lotter-all/lotterbet/betbjkl8.vue
+++ app2/src/components/lotter-all/lotterbet/betbjkl8.vue
@@ -4,24 +4,25 @@
       <li><router-link to="/one" tag="i" class="el-icon-arrow-left"></router-link></li>
       <li>
         <p class="wangfa">玩<br/>法</p>
-        <div class="menu" @click="show = !show">任选一
+        <div class="menu" @click="show = !show">{{titles}}
           <i :class="show ? 'el-icon-caret-top' : 'el-icon-caret-bottom'"></i>
         </div>
-        <div v-show="!show" class="menu-list" v-for="(muens,index) in muen" :key="index">
-          <div class="menu-list-top"><p>{{muens.a}}</p></div>
+        <div class="menu-list">
+          <div v-show="!show">
+            <div class="menu-list-top">
+              <p v-for="(muens,index) in muen" :key="index" @click="menunav($event,index,muens)">{{muens.a}}</p>
+            </div>
+            <ul>
+              <li v-for="(val,index) in muen" :key="index" v-show="index === navs"><p v-for="(item,index) in val.valus" :key="index"  @click="isshow($event,item)">{{item}}</p></li>
+            </ul>
+          </div>
           
-          <!-- <ul><li><span v-for="(val,index) in muens.valus" :key="index">{{val}}</span></li></ul> -->
         </div>
       </li>
       <li><p>北京快乐</p></li>
     </ul>
-    <div class="betbjkl8-content">北京快乐8
-      <div v-for="(muens,index) in muen" :key="index">
-        <p v-show="index === 0">{{muens.a}}</p>
-        <ul>
-          <li  v-show="index === 0"><p v-for="(val,index) in muens.valus" :key="index">{{val}}</p></li>
-        </ul>
-      </div>
+    <div class="betbjkl8-content">
+      qqq
     </div>
   </div>
 </template>
@@ -30,7 +31,9 @@
     data() {
       return {
         show:true,
-        show1:true,
+        show1:false,
+        navs: 0,
+        titles:'任选一',
         muen:[
           {a:'任选',valus:['任选一','任选二','任选三','任选四','任选五','任选六','任选七','任选八','任选九']},
           {a:'趣味',valus:['上下盘','奇偶盘','和值大小单双']},
@@ -38,7 +41,15 @@
       };
     },
     methods: {
-      
+      menunav(event,index,muens){
+        if(muens){
+          this.navs = index;
+        }
+      },
+      isshow(event,item){
+        this.titles = item;
+        this.show = !this.show;
+      }
     }
   }
 </script>
