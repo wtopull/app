4cfdd95ddd41ee0946bc8a18da761c8d1b693074
wtopull <471601494@qq.com>
2018-04-17 20:44:00 +0800

    aaaaa

---
+++ vue-game/vue-game/src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <number-choose v-on:choose="onChoose" :unit="number.unit" :selected-nums="selectedNums" v-for="(number, index) in numbers" :key="index"></number-choose>
+  </div>
+</template>
+
+<script>
+import NumberChoose from './NumberChoose'
+
+export default {
+  name: 'Home',
+  components: {
+    NumberChoose
+  },
+  data () {
+    return {
+      numbers: [
+        { unit: '万位' },
+        { unit: '千位' },
+        { unit: '百位' },
+        { unit: '十位' },
+        { unit: '个位' }
+      ],
+      selectedNums: []
+    }
+  },
+  methods: {
+    onChoose (val, oldVal) {
+      let index = this.selectedNums.indexOf(oldVal)
+      if (index > -1) {
+        this.selectedNums.splice(index, 1)
+      }
+      this.selectedNums.push(val)
+    }
+  }
+}
+</script>
+
+<style>
+.home {
+  border: 1px solid #ddd;
+  border-radius: 6px;
+  width: 780px;
+  padding: 14px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
